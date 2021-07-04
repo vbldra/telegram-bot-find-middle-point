ca5bd11f57bfc0df7416ce18627dacb2a2090f9f
ilona <ilona.avdiushkina@gmail.com>
2021-07-04 21:26:55 +0200

    saving data by chatId

--- bot.js
+++ bot.js
@@ -4,31 +4,40 @@ const { Telegraf } = require("telegraf");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 let locationsData = {};
-let isBotWorking = false;
+// let isBotWorking = false;
 
-bot.start((ctx) => ctx.reply("I will help you to find where to meet"));
+bot.start((ctx) =>
+  ctx.reply(
+    "Hi! I will help you and your friends to find where to meet. Send /help to see more."
+  )
+);
 bot.help((ctx) =>
   ctx.reply(
-    "Send me /wtf to start. Then send your location. When you finished, send me /stop"
+    "Send me /wtf to start. Then send your location. When you finished, send me /find"
   )
 );
 
 bot.command("/wtf", (ctx) => {
-  isBotWorking = true;
-  ctx.reply("Send me your location. When you finished, send me /stop");
+  locationsData[ctx.update.message.chat.id] = {};
+  ctx.reply("Send me your location. When you finished, send me /find");
 });
-bot.on("location", ({ from, message }) => {
-  if (isBotWorking) {
-    locationsData[from.id] = message.location;
+bot.on("location", ({ chat, from, message }) => {
+  const chatId = chat.id.toString();
+  if (locationsData[chatId]) {
+    locationsData[chatId][from.id] = message.location;
   }
 });
-bot.command("/stop", (ctx) => {
-  if (Object.keys(locationsData).length !== 0) {
+bot.command("/find", (ctx) => {
+  const chatId = ctx.update.message.chat.id.toString();
+  if (
+    locationsData[chatId] &&
+    Object.keys(locationsData[chatId]).length !== 0
+  ) {
     (async () => {
       try {
         const response = await axios.post(
           "http://localhost:8080/bot",
-          Object.values(locationsData)
+          Object.values(locationsData[chatId])
         );
         const coordinates = response.data.coordinates;
         ctx.telegram.sendLocation(
@@ -36,14 +45,14 @@ bot.command("/stop", (ctx) => {
           coordinates.latitude,
           coordinates.longitude
         );
-        locationsData = {};
+        delete locationsData[chatId];
       } catch (error) {
         console.log(error);
       }
     })();
   } else {
     ctx.reply(
-      "Send me /wtf to start. Then send your location. When you finished, send me /stop"
+      "Send me /wtf to start. Then send your location. When you finished, send me /find"
     );
   }
 });
