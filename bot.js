3ee10679350f9278fdb28575b34dc3ca146d5138
Ilona Avdiushkina <ilona.avdiushkina@gmail.com>
2022-03-31 16:30:18 +0200

    await functions + inline keyboard

--- bot.js
+++ bot.js
@@ -17,19 +17,22 @@ bot.start(async (ctx) => {
           ctx.message.from.first_name
             ? ctx.message.from.first_name
             : ctx.message.from.username
-        }!\nI will help you to find middle point between locations.\n\nPress button <b>🗺Start</b> to collect all the locations\nPress button <b>👀Help</b> to see more.`,
+        }!\nI will help you to find middle point between locations.\n\nPress button [ 🗺 Send location ] to collect all the locations\nSend <b>/help</b> to see more.`,
         Markup.inlineKeyboard([
-          [
-            Markup.button.callback("🗺Start", "btn_start"),
-            Markup.button.callback("👀Help", "btn_help"),
-          ],
+          [Markup.button.callback("🗺 Send location", "btn_start")],
         ])
       );
     } else if (chatType === "group") {
-      // ctx.reply(
-      //   "Hi!\nI will help you and your friends to find where to meet.\n\n1. Press button <b>🗺Start</b>, or send /wtf to start\n2. Each of you send me location\n3. Press button <b>📍Find middle point</b>, or send /find to calculate center point between you and your friends\n\nPress button <b>👀Help</b> or send /help to see more."
-      //   // menu
-      // );
+      await ctx.replyWithHTML(
+        `Hi, dear ${
+          ctx.message.chat.title
+            ? ctx.message.chat.title
+            : ctx.message.from.username
+        } team!\nI will help you and your friends to find where to meet.\n\nPress button [ 🗺 Send location ] to collect all the locations\nSend <b>/help</b> to see more.`,
+        Markup.inlineKeyboard([
+          [Markup.button.callback("🗺 Send location", "btn_start")],
+        ])
+      );
     } else {
       ctx.reply("Hi. Please add me to private or group chat");
     }
@@ -40,55 +43,79 @@ bot.start(async (ctx) => {
 
 // HELP
 bot.help((ctx) => {
-  // const chatType = ctx.update.message.chat.type;
-  // if (chatType === "private") {
-  //   ctx.reply(
-  //     "1. Send /wtf to start\n2. Send locations\n3. Send /find to calculate middle point\n\nYou can add me to your group channel and I can help you and your friends to find where to meet.\nTo see more you can visit our website (we're working on it...)"
-  //   );
-  // } else if (chatType === "group") {
-  //   ctx.reply(
-  //     "1. Send /wtf to start\n2. Each of you send me location. You can update your location, I will take last one from everybody of you\n3. Send /find to calculate center point between you and your friends\n\nYou can start with me private chat and I can help you to find middle point between locations.\nTo see more you can visit our website (we're working on it...)"
-  //   );
-  // } else {
-  //   ctx.reply("Hi. Please add me to private or group chat");
-  // }
+  const chatType = ctx.update.message.chat.type;
+  if (chatType === "private") {
+    ctx.replyWithHTML(
+      `1. Press button <b>[ 🗺 Send location ]</b>\n2. Send me your locations.\n3. Press button <b>[ 📍 Find middle point ]</b> to calculate the middle point.\n\nYou can add me to your group channel and I can help you and your friends to find where to meet.\nTo see more you can visit our website <a href='https://wirtreffenfreunde.herokuapp.com/'>Wir Treffen Freunde</a>`,
+      Markup.inlineKeyboard([
+        [Markup.button.callback("🗺 Send location", "btn_start")],
+      ])
+    );
+  } else if (chatType === "group") {
+    ctx.replyWithHTML(
+      `1. Press button <b>[ 🗺 Send location ]</b>\n2. Send me the location of everyone who wants to meet. You can correct your location, I will take last coordinates.\n3. Press button <b>[ 📍 Find middle point ]</b> to calculate the middle point between you and your friends.\n\nYou can start with me private chat and I can help you to find middle point between any locations.\nTo see more you can visit our website <a href='https://wirtreffenfreunde.herokuapp.com/'>Wir Treffen Freunde</a>`,
+      Markup.inlineKeyboard([
+        [Markup.button.callback("🗺 Send location", "btn_start")],
+      ])
+    );
+  } else {
+    ctx.reply("Hi. Please add me to private or group chat");
+  }
 });
 
 // MAIN FUNCTIONALITY
 bot.action("btn_start", async (ctx) => {
-  await ctx.answerCbQuery();
-  locationsData[ctx.update.callback_query.message.chat.id] = {};
-  ctx.reply("Send me your location.");
+  const chatId = ctx.update.callback_query.message.chat.id.toString();
+  delete locationsData[chatId]; // deleting all previous locations for the chat
+  try {
+    await ctx.answerCbQuery();
+    locationsData[chatId] = {};
+    ctx.reply("Send me your location.");
+  } catch (error) {
+    console.error(error);
+  }
 });
 
-bot.on("location", (ctx) => {
+bot.on("location", async (ctx) => {
   const chatId = ctx.update.message.chat.id.toString();
   const chatType = ctx.update.message.chat.type;
   const userId = ctx.update.message.from.id;
-  if (locationsData[chatId]) {
-    if (chatType === "private") {
-      !locationsData[chatId][userId]
-        ? (locationsData[chatId][userId] = [ctx.update.message.location])
-        : locationsData[chatId][userId].push(ctx.update.message.location);
-      ctx.reply(
-        `Send me next location ${
-          locationsData[chatId][userId].length > 1
-            ? "or let's find middle point"
-            : ""
-        }`,
-        locationsData[chatId][userId].length > 1 &&
-          Markup.inlineKeyboard([
-            [
-              Markup.button.callback("📍Find middle point", "btn_find"),
-              Markup.button.callback("👀Help", "btn_help"),
-            ],
-          ])
-      );
-    } else if (chatType === "group") {
-      // locationsData[chatId][userId] = ctx.update.message.location; // creating object with coordinates for every user
-    } else {
-      ctx.reply("Hi. Please add me to a private or a group chat");
+  try {
+    if (locationsData[chatId]) {
+      if (chatType === "private") {
+        !locationsData[chatId][userId]
+          ? (locationsData[chatId][userId] = [ctx.update.message.location])
+          : locationsData[chatId][userId].push(ctx.update.message.location);
+        ctx.reply(
+          `Send me next location ${
+            locationsData[chatId][userId].length > 1
+              ? "or let's find middle point"
+              : ""
+          }`,
+          locationsData[chatId][userId].length > 1 &&
+            Markup.inlineKeyboard([
+              [Markup.button.callback("📍 Find middle point", "btn_find")],
+            ])
+        );
+      } else if (chatType === "group") {
+        locationsData[chatId][userId] = ctx.update.message.location;
+        ctx.reply(
+          `Send me next location ${
+            Object.keys(locationsData[chatId]).length > 1
+              ? "or let's find middle point"
+              : ""
+          }`,
+          Object.keys(locationsData[chatId]).length > 1 &&
+            Markup.inlineKeyboard([
+              [Markup.button.callback("📍 Find middle point", "btn_find")],
+            ])
+        );
+      } else {
+        ctx.reply("Hi. Please add me to a private or a group chat");
+      }
     }
+  } catch (error) {
+    console.error(error);
   }
 });
 
@@ -101,35 +128,38 @@ bot.action("btn_find", async (ctx) => {
     if (chatType === "private") {
       dataToSend = locationsData[chatId][chatId];
     } else if (chatType === "group") {
-      dataToSend = locationsData[chatId][userId];
+      dataToSend = Object.values(locationsData[chatId]);
     } else {
       ctx.reply("Hi. Please add me to a private or a group chat");
     }
-    (async () => {
-      try {
-        const response = await axios.post(
-          `${backendLink}`,
-          Object.values(dataToSend)
-        );
-        console.log(dataToSend);
-
-        const coordinates = response.data.coordinates;
-        console.log(coordinates);
-        ctx.telegram.sendLocation(
-          ctx.update.callback_query.message.chat.id,
-          coordinates.latitude,
-          coordinates.longitude
-        );
-        delete locationsData[chatId]; // deleting all previous locations for the chat
-      } catch (error) {
-        console.error(error);
-      }
-    })();
+    try {
+      const response = await axios.post(
+        `${backendLink}`,
+        Object.values(dataToSend)
+      );
+      const coordinates = response.data.coordinates;
+      ctx.replyWithHTML("Your middle point is here:");
+      await ctx.telegram.sendLocation(
+        ctx.update.callback_query.message.chat.id,
+        coordinates.latitude,
+        coordinates.longitude
+      );
+      ctx.replyWithHTML(
+        `Start again?\n\nPress button [ 🗺 Send location ] to collect all the locations\nSend <b>/help</b> to see more.`,
+        Markup.inlineKeyboard([
+          [Markup.button.callback("🗺 Send location", "btn_start")],
+        ])
+      );
+    } catch (error) {
+      console.error(error);
+    }
   } else {
-    ctx
-      .reply
-      // "Send me /wtf to start. Then send your location. When you finished, send me /find"
-      ();
+    ctx.replyWithHTML(
+      `Press button [ 🗺 Send location ] to collect all the locations\nSend <b>/help</b> to see more.`,
+      Markup.inlineKeyboard([
+        [Markup.button.callback("🗺 Send location", "btn_start")],
+      ])
+    );
   }
 });
 
